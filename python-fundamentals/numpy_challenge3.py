d234ee66acdd2a993ab0d207bdd61752f8fcaf88
future-innovator <yashgoenkag@gmail.com>
2026-06-09 21:16:17 -0500

    Complete NumPy Challenge 3

---
+++ python-fundamentals/numpy_challenge3.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+nums = np.array([5, 10, 15, 20, 25])
+
+print(f"The mean of nums is: {nums.mean()}")
+print(f"The max of nums is: {nums.max()}")
+print(f"The min of nums is: {nums.min()}")
+print(f"The sum of nums is: {nums.sum()}")
+
+#The mean represents the average value of a dataset (can be though of as its "center of mass"), skewed by extremely high and low data points
