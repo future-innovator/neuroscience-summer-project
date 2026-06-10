3d6e9b760a299e41b69573f39030e38571a8b98e
future-innovator <yashgoenkag@gmail.com>
2026-06-09 22:00:47 -0500

    Complete Numpy array creation challenges

---
+++ python-fundamentals/numpy_challenge6-10.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+#Challenge 6
+print(np.arange(0,10))
+
+#Challenge 7
+print(np.arange(10, 110, 10))
+
+#Challenge 8
+print(np.zeros(10))
+
+#Challenge 9
+print(np.ones(10))
+
+#Challenge 10
+rng = np.random.default_rng()
+print(rng.uniform(0,1,10))
+
+#Extension
+nums = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(nums)
