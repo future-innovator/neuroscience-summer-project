b94af46ae6570a436bca2278d7c8acfa24016397
future-innovator <yashgoenkag@gmail.com>
2026-06-09 21:08:25 -0500

    Complete NumPy Challenge 2

---
+++ python-fundamentals/numpy_challenge2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+nums = np.array([5, 10, 15, 20, 25])
+
+print(f"nums * 2: {nums *2}")
+
+print(f"nums + 100: {nums + 100}")
+
+print(f"nums squared: {nums ** 2}")
+
+#NumPy does not require a loop for these operations because it is applying the operation to the entire array as opposed to each element one at a times
